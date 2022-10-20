8bcf95d69b52e116bca7ce24fb7730b8cc4f2c25
mohneesh-nishad <mohneesh@noborderz.com>
2022-10-20 19:35:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## Lambda Thumbnail Generation
+
+### 🚀 What is this?
+
+This is the complete source code to accompany the [article here](https://www.norrapscm.com/posts/2021-02-08-generate-thumbnails-in-lambda-from-s3-with-ffmpeg/).
+
+### ⭐ What does it do?
+
+It takes a video that was uploaded to an S3 bucket and generates *n* number of thumbnails from it.
+
+### 💨 How do I use it?
+
+Create a Lambda and use the files in the `src/` folder. You need to read the article to see how it ties in the AWS infrastructure, but basically:
+
+* Create an S3 bucket
+* Create a trigger event to run a Lambda whenever an `.mp4` file is uploaded to that bucket
+* That event triggers the Lambda (created from this source code) which uses FFmpeg to generate *n* thumbnails
+* Those thumbnails are then uploaded to another S3 bucket
+
+### 🎲 Why would I use this over AWS Transcoder?
+
+You wouldn't, necessarily. AWS Transcoder is good but very expensive compared to doing it yourself in Lambda. I have processed a few hundred short videos using the above source code / technique laid out in the article; the cost has been $0.00. AWS Transcoder costs around $0.45 per 60 minutes of video processed.
---
+++ src/generate-thumbnails-from-video.js
@@ -0,0 +1,112 @@
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const { spawnSync } = require("child_process");
+const doesFileExist = require("./does-file-exist");
+const generateTmpFilePath = require("./generate-tmp-file-path");
+
+const ffprobePath = "/opt/bin/ffprobe";
+const ffmpegPath = "/opt/bin/ffmpeg";
+
+const THUMBNAIL_TARGET_BUCKET = "demo-thumbnail-bucket";
+
+module.exports = async (tmpVideoPath, numberOfThumbnails, videoFileName) => {
+    const randomTimes = generateRandomTimes(tmpVideoPath, numberOfThumbnails);
+
+    for(const [index, randomTime] of Object.entries(randomTimes)) {
+        const tmpThumbnailPath = await createImageFromVideo(tmpVideoPath, randomTime);
+
+        if (doesFileExist(tmpThumbnailPath)) {
+            const nameOfImageToCreate = generateNameOfImageToUpload(videoFileName, index);
+            await uploadFileToS3(tmpThumbnailPath, nameOfImageToCreate);
+        }
+    }
+}
+
+const generateRandomTimes = (tmpVideoPath, numberOfTimesToGenerate) => {
+    const timesInSeconds = [];
+    const videoDuration = getVideoDuration(tmpVideoPath);
+
+    for (let x = 0; x < numberOfTimesToGenerate; x++) {
+        const randomNum = getRandomNumberNotInExistingList(timesInSeconds, videoDuration);
+
+        if(randomNum >= 0) {
+            timesInSeconds.push(randomNum);
+        }
+    }
+
+    return timesInSeconds;
+};
+
+const getRandomNumberNotInExistingList = (existingList, maxValueOfNumber) => {
+    for (let attemptNumber = 0; attemptNumber < 3; attemptNumber++) {
+        const randomNum = getRandomNumber(maxValueOfNumber);
+
+        if (!existingList.includes(randomNum)) {
+            return randomNum;
+        }
+    }
+
+    return -1;
+}
+
+const getRandomNumber = (upperLimit) => {
+    return Math.floor(Math.random() * upperLimit);
+};
+
+const getVideoDuration = (tmpVideoPath) => {
+    const ffprobe = spawnSync(ffprobePath, [
+        "-v",
+        "error",
+        "-show_entries",
+        "format=duration",
+        "-of",
+        "default=nw=1:nk=1",
+        tmpVideoPath
+    ]);
+
+    return Math.floor(ffprobe.stdout.toString());
+};
+
+const createImageFromVideo = (tmpVideoPath, targetSecond) => {
+    const tmpThumbnailPath = generateThumbnailPath(targetSecond);
+    const ffmpegParams = createFfmpegParams(tmpVideoPath, tmpThumbnailPath, targetSecond);
+    spawnSync(ffmpegPath, ffmpegParams);
+
+    return tmpThumbnailPath;
+};
+
+const generateThumbnailPath = (targetSecond) => {
+    const tmpThumbnailPathTemplate = "/tmp/thumbnail-{HASH}-{num}.jpg";
+    const uniqueThumbnailPath = generateTmpFilePath(tmpThumbnailPathTemplate);
+    const thumbnailPathWithNumber = uniqueThumbnailPath.replace("{num}", targetSecond);
+
+    return thumbnailPathWithNumber;
+};
+
+const createFfmpegParams = (tmpVideoPath, tmpThumbnailPath, targetSecond) => {
+    return [
+        "-ss", targetSecond,
+        "-i", tmpVideoPath,
+        "-vf", "thumbnail,scale=80:140",
+        "-vframes", 1,
+        tmpThumbnailPath
+    ];
+};
+
+const generateNameOfImageToUpload = (videoFileName, i) => {
+    const strippedExtension = videoFileName.replace(".mp4", "");
+    return `${strippedExtension}-${i}.jpg`;
+};
+
+const uploadFileToS3 = async (tmpThumbnailPath, nameOfImageToCreate) => {
+    const contents = fs.createReadStream(tmpThumbnailPath);
+    const uploadParams = {
+        Bucket: THUMBNAIL_TARGET_BUCKET,
+        Key: nameOfImageToCreate,
+        Body: contents,
+        ContentType: "image/jpg"
+    };
+
+    const s3 = new AWS.S3();
+    await s3.putObject(uploadParams).promise();
+};
